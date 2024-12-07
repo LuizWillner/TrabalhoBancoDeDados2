9b5de64397ae91fde8597f1b3fd5d4d4cd994fda
LuizWillner <luizwillner@gmail.com>
2024-12-07 13:27:49 -0300

    Script para criar usuário chinook

---
+++ scripts/chinook-db-setup/1-create-user.sql
@@ -0,0 +1,15 @@
+ALTER SESSION SET container=XEpdb1;  -- altera o container para o XEPDB1
+
+CREATE USER chinook IDENTIFIED BY senha container=current;  -- usuário chinook criado com senha "senha"
+
+GRANT connect TO chinook;
+GRANT resource TO chinook;
+GRANT create session TO chinook;
+GRANT create table TO chinook;
+GRANT create view TO chinook;
+GRANT create sequence TO chinook;
+grant UNLIMITED TABLESPACE to developer; -- concede ao usuário developer o privilégio 
+-- de usar qualquer quantidade de espaço em disco em todos os tablespaces disponíveis no banco de dados.
+
+-- Adicionar conexão ao banco de dados com usuário "chinook", senha "senha" e nome do serviço "XEPDB1" 
+-- no SQLDeveloper ou DBeaver
