1c9da7cb46662e05456038ad5b190b1a37ec7ecc
LuizWillner <luizwillner@gmail.com>
2024-12-08 14:12:38 -0300

    Popular tabela MediaType

---
+++ scripts/chinook-db-setup/4-insert-into-meidatype.sql
@@ -0,0 +1,9 @@
+-- Executar script abaixo conectado numa sessão com user "chinook" e service name "XEpdb1"
+
+-- Populate MediaType table
+
+INSERT INTO MediaType (MediaTypeId, Name) VALUES (1, 'MPEG audio file');
+INSERT INTO MediaType (MediaTypeId, Name) VALUES (2, 'Protected AAC audio file');
+INSERT INTO MediaType (MediaTypeId, Name) VALUES (3, 'Protected MPEG-4 video file');
+INSERT INTO MediaType (MediaTypeId, Name) VALUES (4, 'Purchased AAC audio file');
+INSERT INTO MediaType (MediaTypeId, Name) VALUES (5, 'AAC audio file');
