eb39f51302d9ba43a2ad7d62b5d13838708b8380
LuizWillner <luizwillner@gmail.com>
2025-01-04 13:32:07 -0300

    questao5.sql criada

---
+++ scripts/questoes/parte1/questao5.sql
@@ -0,0 +1,6 @@
+-- 5. Implemente uma solução através da programação em banco de dados para validar os 
+-- valores de uma coluna que represente uma situação (estado) garantindo que os seus 
+-- valores e suas transições atendam a especificação de um diagrama de transição de 
+-- estados (DTE). Quanto mais genérica e reutilizável for a solução melhor a pontuação 
+-- nessa questão. Junto da solução deverá ser entregue um cenário de teste 
+-- demonstrando o funcionamento da solução.
