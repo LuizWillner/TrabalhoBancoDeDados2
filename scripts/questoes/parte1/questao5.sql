330e20a9a9fa6675072e55fdb836804963ebfa38
MarinaPiragibe <marinapiragibe@id.uff.br>
2025-01-07 14:38:03 -0300

    Adiciona o diagrama de estados e adapta a questão 5 para ele

--- scripts/questoes/parte1/questao5.sql
+++ scripts/questoes/parte1/questao5.sql
@@ -12,11 +12,18 @@ CREATE TABLE StateTransitions (
     PRIMARY KEY (CurrentState, NextState)
 );
 
-INSERT INTO StateTransitions (CurrentState, NextState) VALUES ('Pending', 'Paid');
-INSERT INTO StateTransitions (CurrentState, NextState) VALUES ('Pending', 'Cancelled');
+DROP TABLE StateTransitions CASCADE CONSTRAINTS;
+
+INSERT INTO StateTransitions (CurrentState, NextState) VALUES ('Open', 'Canceled');
+INSERT INTO StateTransitions (CurrentState, NextState) VALUES ('Open', 'Closed');
+INSERT INTO StateTransitions (CurrentState, NextState) VALUES ('Closed', 'Canceled');
+INSERT INTO StateTransitions (CurrentState, NextState) VALUES ('Closed', 'Paid');
+
+SELECT *
+FROM STATETRANSITIONS s 
 
 -- Cria uma nova coluna na tabela 'Invoice' para armazenar o status da fatura
-ALTER TABLE Invoice ADD Status VARCHAR2(50) DEFAULT 'Pending';
+ALTER TABLE Invoice ADD Status VARCHAR2(50) DEFAULT 'Open';
 
 -- Procedure para alterar o status
 CREATE OR REPLACE PROCEDURE UpdateInvoiceStatus(
@@ -52,27 +59,26 @@ BEGIN
     SET Status = status_novo
     WHERE InvoiceId = invoice_id;
 
-    -- Confirmar a alteração (opcional, dependendo do ambiente)
     COMMIT;
 END;
 
--- Teste 1: Transição válida (Pending -> Paid)
+-- Teste 1: Transição válida (Open -> Canceled)
 BEGIN
-    UpdateInvoiceStatus(1, 'Paid');
+    UpdateInvoiceStatus(1, 'Closed');
 END;
 
 -- Teste 2: Transição válida (Pending -> Cancelled)
 BEGIN
-    UpdateInvoiceStatus(2, 'Cancelled');
+    UpdateInvoiceStatus(2, 'Canceled');
 END;
 
--- Teste 3: Transição inválida (Paid -> Cancelled)
+-- Teste 3: Transição inválida (Open -> Paid)
 BEGIN
-    UpdateInvoiceStatus(1, 'Cancelled');
+    UpdateInvoiceStatus(3, 'Paid');
 END;
 
--- Teste 4: Transição inválida (Cancelled -> Pending)
+-- Teste 4: Transição inválida (Closed -> Open)
 BEGIN
-    UpdateInvoiceStatus(2, 'Pending');
+    UpdateInvoiceStatus(1, 'Open');
 END;
 
