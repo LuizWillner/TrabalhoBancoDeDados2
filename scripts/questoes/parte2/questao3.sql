c89233939650cb60dae8ba9f8edf872f9dcc680a
LuizWillner <luizwillner@gmail.com>
2025-01-10 14:37:42 -0300

    REFACTOR: Mudança na tipagem dos params das procedures

--- scripts/questoes/parte2/questao3.sql
+++ scripts/questoes/parte2/questao3.sql
@@ -5,7 +5,7 @@
 
 
 -- Procedure para verificar a data de nascimento de funcionários
-CREATE OR REPLACE PROCEDURE check_employee_birthdate(p_birthdate IN DATE) IS
+CREATE OR REPLACE PROCEDURE check_employee_birthdate(p_birthdate IN employee.birthdate%TYPE) IS
     v_age NUMBER;
 BEGIN
     -- Verificar se a data de nascimento não é uma data futura
@@ -20,32 +20,35 @@ END;
 
 
 -- Criação de um usuário com permissões limitadas
-CREATE USER usuario_limitado IDENTIFIED BY senha;
-GRANT CONNECT TO usuario_limitado;
-GRANT EXECUTE ON check_employee_birthdate TO usuario_limitado;  -- Conceder permissão para executar a procedure
+CREATE USER usuario_de_acesso IDENTIFIED BY senha;
+GRANT CONNECT TO usuario_de_acesso;
+GRANT EXECUTE ON check_employee_birthdate TO usuario_de_acesso;  -- Conceder permissão para executar a procedure
 
 
 -- Procedure para inserir ou atualizar funcionários
 CREATE OR REPLACE PROCEDURE manage_employee(
-    p_employeeid IN NUMBER,
-    p_lastname IN VARCHAR2,
-    p_firstname IN VARCHAR2,
-    p_title IN VARCHAR2,
-    p_reportsto IN NUMBER,
-    p_birthdate IN DATE,
-    p_hiredate IN DATE,
-    p_address IN VARCHAR2,
-    p_city IN VARCHAR2,
-    p_state IN VARCHAR2,
-    p_country IN VARCHAR2,
-    p_postalcode IN VARCHAR2,
-    p_phone IN VARCHAR2,
-    p_fax IN VARCHAR2,
-    p_email IN VARCHAR2
+    p_employeeid IN employee.employeeid%TYPE,
+    p_lastname IN employee.lastname%TYPE,
+    p_firstname IN employee.firstname%TYPE,
+    p_title IN employee.title%TYPE,
+    p_reportsto IN employee.reportsto%TYPE,
+    p_birthdate IN employee.birthdate%TYPE,
+    p_hiredate IN employee.hiredate%TYPE,
+    p_address IN employee.address%TYPE,
+    p_city IN employee.city%TYPE,
+    p_state IN employee.state%TYPE,
+    p_country IN employee.country%TYPE,
+    p_postalcode IN employee.postalcode%TYPE,
+    p_phone IN employee.phone%TYPE,
+    p_fax IN employee.fax%TYPE,
+    p_email IN employee.email%TYPE
 ) IS
 BEGIN
     -- Verificar a data de nascimento
     check_employee_birthdate(p_birthdate);
+    
+     -- reaproveitando a procedure criada na questão 2. Redundante se considerar que já existe trigger pra isso definida na questão 2
+    check_not_future_date(p_hiredate, 'HireDate');
 
     -- Comando MERGE utilizado para inserir ou atualizar o funcionário
     MERGE INTO employee e
@@ -79,4 +82,14 @@ BEGIN
 END;
 
 -- Conceder permissão para executar a procedure manage_employee
-GRANT EXECUTE ON manage_employee TO usuario_limitado;
+GRANT EXECUTE ON manage_employee TO usuario_de_acesso;
+
+-- Numa conexão com usuario_de_acesso, podemos executar a procedure manage_employee:
+BEGIN
+    manage_employee(
+        100, 'Silva', 'João', 'Sales Support Agent', 1, 
+        TO_DATE('2001-09-03 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), -- Se trocar a data de nascimento para uma data futura ou menor que 18 anos, a procedure irá falhar
+        TO_DATE('2020-10-03 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), -- Se trocar a data de contratação para uma data futura, a procedure irá falhar
+        'Rua dos Bobos, 0', 'Calgary', 'AB', 'Canada', 'T2P 2T3', '+1 (403) 000-0000', '+1 (403) 000-0001', 'silvajoao@email.com');
+END;
+
