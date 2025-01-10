5680fab922c44c6a6310aeececaaba68319960dd
LuizWillner <luizwillner@gmail.com>
2025-01-09 21:41:51 -0300

    REFACTOR: Abordagem por procedure somente na questão 4

--- scripts/questoes/parte2/questao4.sql
+++ scripts/questoes/parte2/questao4.sql
@@ -4,34 +4,56 @@
 -- semântica onde o valor Total de um Invoice deve ser igual à soma de UnitPrice * 
 -- Quantity de todos os registros de InvoiceLine relacionados a um Invoice. 
 -- Implementar uma solução que garanta a integridade dessa regra.
-CREATE OR REPLACE PROCEDURE atualizar_total_invoice(p_invoiceid NUMBER) AS
-    v_total NUMBER;
+
+
+-- Procedure para inserir ou atualizar invoiceline. Atualiza coluna total em invoice de acordo com a regra semântica
+CREATE OR REPLACE PROCEDURE manage_invoiceline(
+    p_invoicelineid IN invoiceline.INVOICELINEID%TYPE,
+    p_invoiceid IN invoiceline.INVOICEID%TYPE,
+    p_trackid IN invoiceline.TRACKID%TYPE,
+    p_unitprice IN invoiceline.UNITPRICE%TYPE,
+    p_quantity IN invoiceline.QUANTITY%TYPE
+) IS
+	v_total invoice.TOTAL%TYPE;
 BEGIN
-    -- Calcula o total da invoice com base nas linhas de invoice
-    SELECT SUM(quantity * unitprice)
+    -- Comando MERGE utilizado para inserir ou atualizar invoiceline
+    MERGE INTO invoiceline il
+    USING (SELECT p_invoicelineid AS invoicelineid FROM dual) src
+    ON (il.invoicelineid = src.invoicelineid)
+    WHEN MATCHED THEN
+        UPDATE SET
+            il.invoiceid = p_invoiceid,
+            il.trackid = p_trackid,
+            il.unitprice = p_unitprice,
+            il.quantity = p_quantity
+    WHEN NOT MATCHED THEN
+        INSERT 
+        	(invoicelineid, invoiceid, trackid, unitprice, quantity)
+        VALUES 
+       		(p_invoicelineid, p_invoiceid, p_trackid, p_unitprice, p_quantity);
+       	
+    -- Atualiza o total em invoice
+    SELECT SUM(il.unitprice * il.quantity)
     INTO v_total
-    FROM invoiceline
-    WHERE invoiceid = p_invoiceid;
-    
-    -- Atualiza o total na tabela invoice
-    UPDATE invoice
+    FROM INVOICE i
+    JOIN INVOICELINE il ON i.invoiceid = il.invoiceid  
+    WHERE i.invoiceid = p_invoiceid
+	GROUP BY i.invoiceid;
+
+	UPDATE invoice
     SET total = v_total
     WHERE invoiceid = p_invoiceid;
+END;
 
-END atualizar_total_invoice;
+DROP PROCEDURE ATUALIZAR_TOTAL_INVOICE;
+
+DROP TRIGGER TRIGGER_ATUALIZAR_TOTAL_INVOICE;
 
 
-CREATE OR REPLACE TRIGGER trigger_atualizar_total_invoice
-AFTER INSERT OR UPDATE OF quantity, unitprice ON invoiceline
 BEGIN
-    -- Chama a procedure para atualizar o total da invoice
-    -- A procedure atualizará o total com base no invoiceid
-    FOR r IN (SELECT DISTINCT invoiceid FROM invoiceline WHERE invoiceid IS NOT NULL) LOOP
-        atualizar_total_invoice(r.invoiceid);
-    END LOOP;
-END trigger_atualizar_total_invoice;
+	manage_invoiceline(10000, 1, 130, 30.0, 1);
+END;
 
 
-update invoiceline
-set unitprice = 0.99
-where invoicelineid = 1;
+-- Conceder permissão para executar a procedure manage_invoiceline
+GRANT EXECUTE ON manage_invoiceline TO usuario_limitado;
